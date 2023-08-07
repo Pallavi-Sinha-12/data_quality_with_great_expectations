57b99cf9f0e69e3ddac195fe998a912a8fa747ec
Pallavi-Sinha-TW <pallavi.sinha@thoughtworks.com>
2023-08-07 10:35:48 +0530

    Refactored config and added new column

--- data_quality/DataQuality.py
+++ data_quality/DataQuality.py
@@ -11,7 +11,7 @@ class DataQuality:
         self.config_path = config_path
 
     def rule_mapping(self, dq_rule):
-        return{"check_null" : "NotNullExpectation", "check_unique" : "UniqueExpectation", "check_if_values_in_list" : "ValuesInListExpectation"}[dq_rule]
+        return{"check_if_not_null" : "NotNullExpectation", "check_if_unique" : "UniqueExpectation", "check_if_values_in_list" : "ValuesInListExpectation"}[dq_rule]
 
     def _get_expectation(self):
         class_obj = globals()[self.rule_mapping()]
@@ -28,13 +28,11 @@ class DataQuality:
         ge_df = self.convert_to_ge_df()
         config = self.read_config()
         for column in config["columns"]:
+            if column["dq_rule(s)"] is None:
+                continue
             for dq_rule in column["dq_rule(s)"]:
-                rule_name = dq_rule["rule_name"]
-                print(rule_name)
-                # get a class object
                 expectation_obj = globals()[self.rule_mapping(dq_rule["rule_name"])]
-                print(column["src_column"], dq_rule["rule_dimension"], dq_rule["add_info"], rule_name)
-                expectation_instance = expectation_obj(column["src_column"], dq_rule["rule_dimension"], dq_rule["add_info"])
+                expectation_instance = expectation_obj(column["column_name"], dq_rule["rule_dimension"], dq_rule["add_info"])
                 expectation_instance.test(ge_df)
 
         dq_results = ge_df.validate()
--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from utils.utils import create_df_from_dq_results
 spark = SparkSession.builder.appName("DataQuality").getOrCreate()
 
 student_df = spark.createDataFrame([
-    (1, "Maths"),
-    (2, "History"),
-    (3, "Science"),
-    (4, "Maths"),
-    (5, "Science"),
-    (6, "Maths"),
-    (7, "Hindi"),
-    (8, "Sanskrit")], ["roll_no", "subject"])
+    (1,"Ram","Kumar", "Maths"),
+    (2,"Shyam","Kumar", "History"),
+    (3,"Mohan", None , "Science"),
+    (4,"Sohan","Singh", "Maths"),
+    (5,"Rohini","Kumari", "Science"),
+    (6,"Raj", "Kumar", "Maths"),
+    (7,"Meena", None , "Hindi"),
+    (8,"Rani", "Kumari", "Sanskrit")], ["roll_no", "first_name", "last_name", "subject"])
 
 dq = DataQuality(student_df, "config/config.json")
 dq_results = dq.run_test()
